afcd4d794bdcf022613fd8fff93bd5cfb7aaf8a8
ritikyadav55079-hash <ritikyadav55079@gmail.com>
2025-11-01 09:53:18 +0530

    Create Java Servlet LoginServlet.java

---
+++ Java Servlet LoginServlet.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+
+public class LoginServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+        throws ServletException, IOException {
+
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+
+        // Hardcoded credentials (can be replaced with DB check)
+        if ("admin".equals(username) && "1234".equals(password)) {
+            out.println("<h2>Welcome, " + username + "!</h2>");
+        } else {
+            out.println("<h3>Invalid username or password.</h3>");
+            out.println("<a href='login.html'>Try again</a>");
+        }
+    }
+}
